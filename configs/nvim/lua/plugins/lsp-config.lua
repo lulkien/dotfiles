abb04a8688b0c1c9c1fe48115da250499c6dbe90
lulkien <kien.luuhoang.arch@gmail.com>
2023-12-31 14:19:28 +0700

    Change nvim config

---
+++ configs/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,48 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗"
+                    }
+                }
+            })
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "bashls",
+                    "clangd",
+                    "cmake",
+                    "cssls",
+                    "jsonls",
+                    "tsserver",
+                    "lua_ls",
+                    "pyright",
+                    "rust_analyzer",
+                    "slint_lsp",
+                }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.tsserver.setup({})
+            lspconfig.bashls.setup({})
+
+            -- Keys binding
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        end
+    }
+}
--- configs/nvim/lua/plugins/neo-tree.lua
+++ configs/nvim/lua/plugins/neo-tree.lua
@@ -8,5 +8,6 @@ return {
     },
     config = function ()
         vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle<CR>", {})
+        vim.keymap.set("n", "<leader>e", ":Neotree focus<CR>", {})
     end
 }
