a78aa6b3b12f816b0cb51dd01ecb7a9db4576cdf
lulkien <kien.luuhoang.arch@gmail.com>
2024-02-04 01:16:40 +0700

    Add Doom Easter Egg

--- home/.config/fish/conf.d/kfc_common.fish
+++ home/.config/fish/conf.d/kfc_common.fish
@@ -10,8 +10,9 @@ end
 
 # Fish builtin variables
 set -g fish_color_valid_path
+set -g fish_term24bit 1
 set -g fish_greeting
-set -g fish_term24bit               1
+# set -g fish_greeting    $KFC_GREEN_B"ARMORED RESPONSE COALITION - TOGETHER WE SURVIVE"
 
 # Append some paths into PATH variable
 set -a PATH     $HOME/.cargo/bin
