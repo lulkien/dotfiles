57afb4e29405a336a77148dcea187fc0ef6cfbe2
lulkien <kien.luuhoang.arch@gmail.com>
2024-02-07 21:18:16 +0700

    Change hyprland config

--- home/.config/hypr/scripts/start-xdph.sh
+++ home/.config/hypr/scripts/start-xdph.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
 sleep 1
-killall -e xdg-desktop-portal-hyprland
-killall -e xdg-desktop-portal-wlr
-killall -e xdg-desktop-portal
-
-# Start XDPH
-/usr/lib/xdg-desktop-portal-hyprland &
+systemctl restart --user xdg-desktop-portal-hyprland.service
 
 sleep 2
-/usr/lib/xdg-desktop-portal &
+systemctl restart --user xdg-desktop-portal.service
