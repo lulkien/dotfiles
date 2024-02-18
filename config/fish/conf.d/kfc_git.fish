1ded896c96b43c2daf894390451f529b6825c055
lulkien <kien.luuhoang.arch@gmail.com>
2024-02-19 01:36:26 +0700

    Change fish config

--- config/fish/conf.d/kfc_git.fish
+++ config/fish/conf.d/kfc_git.fish
@@ -1,14 +1,23 @@
-abbr -a gco 'git checkout'
-abbr -a gcm 'git commit'
-abbr -a gca 'git commit --amend'
-abbr -a gcn 'git commit --amend --no-edit'
-abbr -a grs 'git clean -f -d && git reset --hard HEAD^^ && git pull --rebase'
-abbr -a gpo 'git push origin'
-abbr -a gpl 'git pull --rebase'
-abbr -a gcl 'git clone'
-abbr -a gst 'git status'
-abbr -a gdf 'git diff'
-abbr -a gad 'git add'
-abbr -a glo 'git log --oneline'
-abbr -a gpp 'git_push_now'
-abbr -a gpy 'git_push_now --yolo'
+# Repos
+abbr -a gcl     'git clone'
+abbr -a gco     'git checkout'
+abbr -a gpl     'git pull --rebase'
+abbr -a grs     'git clean -f -d && git reset --hard HEAD^^ && git pull --rebase'
+
+# Commit
+abbr -a gad     'git add'
+abbr -a gcm     'git commit'
+abbr -a gca     'git commit --amend'
+abbr -a gcan    'git commit --amend --no-edit'
+abbr -a gta     'git tag -a'
+
+# Status
+abbr -a gst     'git status'
+abbr -a gdf     'git diff'
+abbr -a glo     'git log --oneline'
+abbr -a gde     'git describe'
+
+# Push
+abbr -a gpo     'git push origin'
+abbr -a gpp     'git_push_now'
+abbr -a gppp    'git_push_now --yolo'
