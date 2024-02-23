3fb4aaa4c19b918684b467061e284864c3e2befb
lulkien <kien.luuhoang.arch@gmail.com>
2024-02-24 00:28:59 +0700

    Change nvim config

--- config/nvim/lua/custom/plugins.lua
+++ config/nvim/lua/custom/plugins.lua
@@ -64,19 +64,6 @@ local M = {
 			return M
 		end,
 	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
 }
 
 return M
