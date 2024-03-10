f0d9790ec3545fb2cf6b570e029ece737b5d47bc
lulkien <kien.luuhoang.arch@gmail.com>
2024-03-10 18:44:06 +0700

    Fix some dumb issue on volume control script

--- config/hypr/scripts/volume_ctrl.sh
+++ config/hypr/scripts/volume_ctrl.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 CMD="$1"
+
 VOLUME=0
 MUTED=false
+
+# For dunstify
 NOTIFY_ID=865863
-NOTIFY_TITLE="Volume changed"
-VOLUME_ICO='audio-volume-medium'
+NOTIFY_TITLE=
+NOTIFY_TIMEOUT=2500
+NOTIFY_ICON=
 
 if [[ -z "$CMD" || ! $(command -v wpctl) ]]; then
     exit 1
@@ -29,44 +33,26 @@ parse_volume() {
     fi
 }
 
-increase_volume() {
-    parse_volume
-    wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+
-}
-
-decrease_volume() {
-    parse_volume
-    wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%-
-}
-
-toggle_mute() {
-    parse_volume
-    if [[ $MUTED = true && $VOLUME -gt 50 ]]; then
-        wpctl set-volume @DEFAULT_AUDIO_SINK@ 50%
-    fi
-    wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-}
-
 parse_notify_title() {
     if [[ $MUTED = true ]]; then
-        NOTIFY_TITLE="$1"
+        NOTIFY_TITLE="Muted"
     else
-        NOTIFY_TITLE="$2"
+        NOTIFY_TITLE="Volume"
     fi
 }
 
 parse_volume_icon() {
     if [[ $MUTED = true || $VOLUME -eq 0 ]]; then
-        VOLUME_ICO='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-muted.svg'
+        NOTIFY_ICON='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-muted.svg'
         return
     fi
 
     if [[ $VOLUME -le 33 ]]; then
-        VOLUME_ICO='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-low.svg'
+        NOTIFY_ICON='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-low.svg'
     elif [[ $VOLUME -le 66 ]]; then
-        VOLUME_ICO='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-medium.svg'
+        NOTIFY_ICON='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-medium.svg'
     else
-        VOLUME_ICO='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-high.svg'
+        NOTIFY_ICON='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-high.svg'
     fi
 }
 
@@ -74,36 +60,34 @@ send_notify() {
     if [[ ! $(command -v dunstify) ]]; then
         exit 1
     fi
-    dunstify --replace=$NOTIFY_ID --icon=$VOLUME_ICO --hints=int:value:"$VOLUME%" "$NOTIFY_TITLE" "$VOLUME%"
+    dunstify --replace=$NOTIFY_ID --timeout=$NOTIFY_TIMEOUT --icon=$NOTIFY_ICON --hints=int:value:"$VOLUME%" "$NOTIFY_TITLE" "$VOLUME%"
 }
 
 do_process() {
+    parse_volume
+
     case "$CMD" in
         '+' | 'inc' | 'INC')
-            increase_volume
-            parse_volume
-            parse_notify_title "Muted" "Volume changed"
-            parse_volume_icon
-            send_notify
+            wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+
             ;;
         '-' | 'dec' | 'DEC')
-            decrease_volume
-            parse_volume
-            parse_notify_title "Muted" "Volume changed"
-            parse_volume_icon
-            send_notify
+            wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%-
             ;;
         '0' | 'toggle' | 'TOGGLE')
-            toggle_mute
-            parse_volume
-            parse_notify_title "Muted" "Volume"
-            parse_volume_icon
-            send_notify
+            if [[ $MUTED = true && $VOLUME -gt 50 ]]; then
+                wpctl set-volume @DEFAULT_AUDIO_SINK@ 50%
+            fi
+            wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
             ;;
         *)
-            exit 1
+            return
             ;;
     esac
+
+    parse_volume
+    parse_notify_title
+    parse_volume_icon
+    send_notify
 }
 
 do_process
