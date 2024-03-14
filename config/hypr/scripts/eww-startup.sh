aba91f5ec088fe46240f2438f21845d2ae619e35
lulkien <kien.luuhoang.arch@gmail.com>
2024-03-14 23:59:10 +0700

    Change eww config

--- config/hypr/scripts/eww-startup.sh
+++ config/hypr/scripts/eww-startup.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-if [[ ! $(command -v eww ) ]]; then
-    exit 1
+if [[ ! $(command -v eww) ]]; then
+	exit 1
 fi
 
 eww kill &
 eww -c ~/.config/eww/widgets/power-overlay kill &
-eww -c ~/.config/eww/widgets/quick-control kill &
+eww -c ~/.config/eww/widgets/system-menu kill &
 
 sleep 0.2
 
 eww daemon
 eww -c ~/.config/eww/widgets/power-overlay daemon
-eww -c ~/.config/eww/widgets/quick-control daemon
+eww -c ~/.config/eww/widgets/system-menu daemon
 
 sleep 0.1
 
@@ -21,8 +21,8 @@ attempt=0
 
 # Wait until hyprvisor start
 until pidof hyprvisor || [[ $attempt -ge $max_check_attempts ]]; do
-    sleep 0.1
-    ((attempt++))
+	sleep 0.1
+	((attempt++))
 done
 
 eww open bar
