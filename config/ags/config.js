bff9b76761e3b546acaed00d106e29d19990a756
lulkien <kien.luuhoang.arch@gmail.com>
2024-03-17 12:33:18 +0700

    Add bluetooth to system indicator

--- config/ags/config.js
+++ config/ags/config.js
@@ -2,6 +2,7 @@ const hyprland = await Service.import("hyprland");
 const notifications = await Service.import("notifications");
 const mpris = await Service.import("mpris");
 const audio = await Service.import("audio");
+const bluetooth = await Service.import("bluetooth");
 const systemtray = await Service.import("systemtray");
 import * as Utils from "resource:///com/github/Aylur/ags/utils.js";
 
@@ -13,7 +14,7 @@ const COMPILED_STYLE_DIR = "/tmp/ags";
 
 function ArchButton() {
     return Widget.Button({
-        class_name: "arch-button",
+        class_names: ["arch-button", "left-item", "bar-item", "box-item"],
         cursor: "pointer",
         child: Widget.Label("󰣇"),
     });
@@ -44,102 +45,17 @@ function Workspaces() {
     });
 
     return Widget.Box({
-        class_name: "workspaces",
+        class_names: ["workspaces", "left-item", "bar-item", "box-item"],
         children: workspaces,
-    });
-}
-
-function ClientTitle() {
-    const title = hyprland.active.client
-        .bind("title")
-        .as((title) =>
-            title.length >= 50 ? title.substring(0, 47) + "..." : title,
-        );
-
-    return Widget.Label({
-        class_name: "client-title",
-        label: title,
     });
 }
 
 function Clock() {
     return Widget.Label({
-        class_name: "clock",
+        class_names: ["clock", "center-item", "bar-item", "box-item"],
         label: date.bind(),
     });
 }
-
-// we don't need dunst or any other notification daemon
-// because the Notifications module is a notification daemon itself
-function Notification() {
-    const popups = notifications.bind("popups");
-    return Widget.Button({
-        class_name: `notification${popups.as((p) => p.length > 0) ? "-new" : "-empty"}`,
-        cursor: "pointer",
-        child: Widget.Label({
-            label: popups.as((p) => (p.length > 0 ? "󱅫" : "󰂚")),
-        }),
-    });
-}
-//
-// function Media() {
-//     const label = Utils.watch("", mpris, "player-changed", () => {
-//         if (mpris.players[0]) {
-//             const { track_artists, track_title } = mpris.players[0];
-//             return `${track_artists.join(", ")} - ${track_title}`;
-//         } else {
-//             return "Nothing is playing";
-//         }
-//     });
-//
-//     return Widget.Button({
-//         class_name: "media",
-//         on_primary_click: () => mpris.getPlayer("")?.playPause(),
-//         on_scroll_up: () => mpris.getPlayer("")?.next(),
-//         on_scroll_down: () => mpris.getPlayer("")?.previous(),
-//         child: Widget.Label({ label }),
-//     });
-// }
-
-// function Volume() {
-//     const icons = {
-//         101: "overamplified",
-//         67: "high",
-//         34: "medium",
-//         1: "low",
-//         0: "muted",
-//     };
-//
-//     function getIcon() {
-//         const icon = audio.speaker.is_muted
-//             ? 0
-//             : [101, 67, 34, 1, 0].find(
-//                   (threshold) => threshold <= audio.speaker.volume * 100,
-//               );
-//
-//         return `audio-volume-${icons[icon]}-symbolic`;
-//     }
-//
-//     const icon = Widget.Icon({
-//         icon: Utils.watch(getIcon(), audio.speaker, getIcon),
-//     });
-//
-//     const slider = Widget.Slider({
-//         hexpand: true,
-//         draw_value: false,
-//         on_change: ({ value }) => (audio.speaker.volume = value),
-//         setup: (self) =>
-//             self.hook(audio.speaker, () => {
-//                 self.value = audio.speaker.volume || 0;
-//             }),
-//     });
-//
-//     return Widget.Box({
-//         class_name: "volume",
-//         css: "min-width: 180px",
-//         children: [icon, slider],
-//     });
-// }
 
 function SysTray() {
     const items = systemtray.bind("items").as((items) =>
@@ -159,22 +75,36 @@ function SysTray() {
     );
 
     return Widget.Box({
-        class_name: "systemtray",
+        class_names: ["systemtray", "right-item", "bar-item", "box-item"],
         children: items,
+    });
+}
+
+function BluetoothIndicator() {
+    return Widget.Button({
+        class_names: ["bluetooth-indicator", "system-indicator-button"],
+        child: Widget.Label("󰂯"),
+        on_clicked: () => {
+            Utils.execAsync(["blueman-manager"]);
+        },
+    });
+}
+
+function SysIndicator() {
+    return Widget.Box({
+        class_names: ["system-indicator", "right-item", "bar-item", "box-item"],
+        children: [BluetoothIndicator()],
     });
 }
 
 function Left() {
     return Widget.Box({
-        spacing: 8,
-        children: [ArchButton(), Workspaces(), ClientTitle()],
+        children: [ArchButton(), Workspaces()],
     });
 }
 
 function Center() {
     return Widget.Box({
-        spacing: 8,
-        // children: [Media(), Notification()],
         children: [Clock()],
     });
 }
@@ -182,8 +112,7 @@ function Center() {
 function Right() {
     return Widget.Box({
         hpack: "end",
-        spacing: 8,
-        children: [SysTray(), Notification()],
+        children: [SysTray(), SysIndicator()],
     });
 }
 
@@ -203,7 +132,6 @@ function Bar(monitor = 0) {
 }
 
 // Parse config
-
 async function applyStyle() {
     Utils.exec(`mkdir -p ${COMPILED_STYLE_DIR}`);
     Utils.exec(
