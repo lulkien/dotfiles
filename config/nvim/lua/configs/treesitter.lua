1440a97df8f901b3f699218664fe5b234169b8a4
lulkien <kien.luuhoang.arch@gmail.com>
2024-03-29 06:26:44 +0700

    Change nvim config

---
+++ config/nvim/lua/configs/treesitter.lua
@@ -0,0 +1,27 @@
+local M = {
+	ensure_installed = {
+		"bash",
+		"fish",
+		"c",
+		"cpp",
+		"rust",
+		"slint",
+		"yuck",
+		"html",
+		"css",
+		"scss",
+		"lua",
+		"python",
+		"qmljs",
+		"qmldir",
+		"cmake",
+		"make",
+		"markdown",
+		"hyprlang",
+		"json",
+		"jsonc",
+		"yaml",
+	},
+}
+
+return M
