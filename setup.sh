0f4a1c80ebbaace6c16ceb884728b7b2f211aad5
lulkien <kien.luuhoang.arch@gmail.com>
2024-04-17 21:56:43 +0700

    Big change

--- setup.sh
+++ setup.sh
@@ -2,48 +2,37 @@
 
 # Env
 PATH=/opt/homebrew/bin:$PATH
-is_macos=$(test $(uname -s) = 'Darwin' && echo true || echo false)
-is_root_user=$(test $USER = root && echo true || echo false)
+IS_DARWIN=$(test $(uname -s) = 'Darwin' && echo true || echo false)
+IS_ROOT=$(test $USER = root && echo true || echo false)
 
 # Source path
-script_path=$(realpath "$(dirname $0)")
-all_dots_dir=${script_path}/config
-stow_home_dir=${script_path}/target_home
-stow_conf_dir=${stow_home_dir}/.config
+SCRIPT_PATH=$(realpath "$(dirname $0)")
+ALL_CONFIGS=${SCRIPT_PATH}/configs
 
 # Config home
-user_conf_dir=$HOME/.config
+USER_CONFIGS=$HOME/.config
 
-distro_target=
-dots=
+TARGET=
+CONFIGS=
 
 setup_dotfiles() {
-	echo "[INFO] Setup dotfiles for $distro_target"
+	if [[ -n $TARGET ]]; then
+		echo "[INFO] Setup dotfiles for $TARGET."
+	else
+		echo "[INFO] Setup dotfiles."
+	fi
 	echo "--------------------------------------"
 
-	if test $(pwd) != $script_path; then
-		echo "[DEBUG] cd $script_path"
-		cd ${script_path}
+	if test $(pwd) != $SCRIPT_PATH; then
+		echo "[DEBUG] cd $SCRIPT_PATH"
+		cd ${SCRIPT_PATH}
 		echo "--------------------------------------"
 	fi
 
-	echo "[INFO] Remake target config"
-	if test -d ${stow_home_dir}; then
-		rm -r ${stow_home_dir}
-	fi
-	mkdir -p $stow_conf_dir
-
-	for conf in "${dots[@]}"; do
-		local conf_path_real=$all_dots_dir/$conf
-		echo "[DEBUG] Link: $conf_path_real -> $stow_conf_dir"
-		ln -s $conf_path_real $stow_conf_dir
-	done
-	echo "--------------------------------------"
-
 	echo "[INFO] Backup or unlink old config"
-	for conf in "${dots[@]}"; do
-		local conf_path=$user_conf_dir/$conf
-		local conf_path_bak=${user_conf_dir}/${conf}.bak
+	for config in "${CONFIGS[@]}"; do
+		local conf_path=${USER_CONFIGS}/${config}
+		local conf_path_bak=${USER_CONFIGS}/${config}.bak
 
 		if [[ -L $conf_path ]]; then
 			echo "[DEBUG] unlink $conf_path"
@@ -56,65 +45,24 @@ setup_dotfiles() {
 	done
 	echo "--------------------------------------"
 
-	echo -n "[INFO] Do you want to setup NvChad? [y/N] "
-	read nvchad_confirm
-	case "$nvchad_confirm" in
-	'y' | 'Y')
-		setup_nvchad
-		;;
-	*)
-		echo "Hmm, nah."
-		;;
-	esac
+	echo "[INFO] Symlink new configs"
+	for config in "${CONFIGS[@]}"; do
+		echo "[DEBUG] Create symlink: ${USER_CONFIGS}/${config} -> ${ALL_CONFIGS}/${config}"
+		ln -s ${ALL_CONFIGS}/${config} ${USER_CONFIGS}/${config}
+	done
 	echo "--------------------------------------"
-
-	echo "[INFO] Start stow --verbose=2 --dir=$stow_home_dir --target=$HOME ."
-	echo
-	stow --verbose=2 --dir=$stow_home_dir --target=$HOME .
-	echo "--------------------------------------"
-
-	# if [[ "$TARGET" = 'Hyprland' ]]; then
-	# 	echo "[INFO] Init submodules for Hyprland"
-	# 	git submodule update --init --recursive
-	# 	echo "--------------------------------------"
-	#
-	# 	echo "[WARN] Please install all submodules for Hyprland."
-	# fi
 
 	echo
 	echo "[INFO] Completed."
 }
 
-setup_nvchad() {
-	local nvchad_dot_conf=$all_dots_dir/nvim
-	local nvchad_conf_dir=$user_conf_dir/nvim
-	local nvchad_conf_bak=$user_conf_dir/nvim.bak
-
-	if [[ -L $nvchad_conf_dir ]]; then
-		echo "[DEBUG] unlink $nvchad_conf_dir"
-		unlink $nvchad_conf_dir
-	elif [[ -d $nvchad_conf_dir ]]; then
-		rm -rf $nvchad_conf_bak
-		echo "[DEBUG] Rename: $nvchad_conf_dir -> $nvchad_conf_bak"
-		mv $nvchad_conf_dir $nvchad_conf_bak
-	fi
-
-	echo "[DEBUG] Link: $nvchad_dot_conf -> $stow_conf_dir"
-	ln -s $nvchad_dot_conf $stow_conf_dir
-}
-
 main() {
-	if ! command -v stow &>/dev/null; then
-		echo "[ERROR] stow: command not found. Please install stow first."
-		return 1
-	fi
-
-	if [[ ${is_root_user} = true ]]; then
-		dots=("fish")
-		distro_target='Root'
-	elif [[ ${is_macos} = true ]]; then
-		dots=("fish" "alacritty")
-		distro_target='MacOS'
+	if [[ ${IS_ROOT} = true ]]; then
+		CONFIGS=("fish")
+		TARGET='Root'
+	elif [[ ${IS_DARWIN} = true ]]; then
+		CONFIGS=("fish" "alacritty")
+		TARGET='Darwin'
 	else
 		echo "Target list:"
 		echo "1: For Hyprland           (everything you need)"
@@ -126,16 +74,15 @@ main() {
 
 		case "${answer}" in
 		1)
-			dots=("hypr" "fish" "ags" "wofi" "alacritty" "dunst")
-			distro_target='Hyprland'
+			CONFIGS=("hypr" "fish" "ags" "wofi" "alacritty" "dunst" "nvim")
+			TARGET='Hyprland'
 			;;
 		2)
-			dots=("fish" "kitty")
-			distro_target='General'
+			CONFIGS=("fish" "kitty" "nvim")
 			;;
 		3)
-			dots=("fish")
-			distro_target='WSL'
+			CONFIGS=("fish")
+			TARGET='WSL'
 			;;
 		*)
 			echo "Canceled."
@@ -144,6 +91,7 @@ main() {
 		esac
 	fi
 
+	echo
 	setup_dotfiles
 }
 
