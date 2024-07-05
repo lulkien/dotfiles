04ad10fc0ad19d5fdf24d5ec40232fd3c784d457
lulkien <kien.luuhoang.arch@gmail.com>
2024-07-05 21:03:44 +0700

    Change config hyprland

--- configs/hypr/scripts/volume_ctrl.sh
+++ configs/hypr/scripts/volume_ctrl.sh
@@ -12,82 +12,82 @@ NOTIFY_TIMEOUT=2500
 NOTIFY_ICON=
 
 if [[ -z "$CMD" || ! $(command -v wpctl) ]]; then
-	exit 1
+    exit 1
 fi
 
 parse_volume() {
-	# Extract volume value
-	local volume_data=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
-	if [[ -z "$volume_data" ]]; then
-		exit 1
-	fi
+    # Extract volume value
+    local volume_data=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+    if [[ -z "$volume_data" ]]; then
+        exit 1
+    fi
 
-	local volume=$(echo $volume_data | cut -d ' ' -f 2)
-	local mute_grep=$(echo $volume_data | grep 'MUTED')
+    local volume=$(echo $volume_data | cut -d ' ' -f 2)
+    local mute_grep=$(echo $volume_data | grep 'MUTED')
 
-	VOLUME=$(awk "BEGIN {print $volume * 100}")
-	if [[ -z "$mute_grep" ]]; then
-		MUTED=false
-	else
-		MUTED=true
-	fi
+    VOLUME=$(awk "BEGIN {print $volume * 100}")
+    if [[ -z "$mute_grep" ]]; then
+        MUTED=false
+    else
+        MUTED=true
+    fi
 }
 
 parse_notify_title() {
-	if [[ $MUTED = true ]]; then
-		NOTIFY_TITLE="Muted"
-	else
-		NOTIFY_TITLE="Volume"
-	fi
+    if [[ $MUTED = true ]]; then
+        NOTIFY_TITLE="Muted"
+    else
+        NOTIFY_TITLE="Volume"
+    fi
 }
 
 parse_volume_icon() {
-	if [[ $MUTED = true || $VOLUME -eq 0 ]]; then
-		NOTIFY_ICON='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-muted.svg'
-		return
-	fi
+    if [[ $MUTED = true || $VOLUME -eq 0 ]]; then
+        NOTIFY_ICON='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-muted.svg'
+        return
+    fi
 
-	if [[ $VOLUME -le 33 ]]; then
-		NOTIFY_ICON='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-low.svg'
-	elif [[ $VOLUME -le 66 ]]; then
-		NOTIFY_ICON='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-medium.svg'
-	else
-		NOTIFY_ICON='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-high.svg'
-	fi
+    if [[ $VOLUME -le 33 ]]; then
+        NOTIFY_ICON='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-low.svg'
+    elif [[ $VOLUME -le 66 ]]; then
+        NOTIFY_ICON='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-medium.svg'
+    else
+        NOTIFY_ICON='/usr/share/icons/Paper/48x48/notifications/notification-audio-volume-high.svg'
+    fi
 }
 
 send_notify() {
-	if [[ ! $(command -v dunstify) ]]; then
-		exit 1
-	fi
-	# dunstify --replace=$NOTIFY_ID --timeout=$NOTIFY_TIMEOUT --icon=$NOTIFY_ICON --hints=int:value:"$VOLUME%" "$NOTIFY_TITLE" "$VOLUME%"
+    if [[ ! $(command -v dunstify) ]]; then
+        exit 1
+    fi
+    dunstify --replace=$NOTIFY_ID --timeout=$NOTIFY_TIMEOUT --icon=$NOTIFY_ICON --hints=int:value:"$VOLUME%" "$NOTIFY_TITLE" "$VOLUME%"
 }
 
 do_process() {
-	parse_volume
+    parse_volume
 
-	case "$CMD" in
-	'+' | 'inc' | 'INC')
-		wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+
-		;;
-	'-' | 'dec' | 'DEC')
-		wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%-
-		;;
-	'0' | 'toggle' | 'TOGGLE')
-		if [[ $MUTED = true && $VOLUME -gt 50 ]]; then
-			wpctl set-volume @DEFAULT_AUDIO_SINK@ 50%
-		fi
-		wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-		;;
-	*)
-		return
-		;;
-	esac
+    case "$CMD" in
+    '+' | 'inc' | 'INC')
+        wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+
+        ;;
+    '-' | 'dec' | 'DEC')
+        wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%-
+        ;;
+    '0' | 'toggle' | 'TOGGLE')
+        if [[ $MUTED = true && $VOLUME -gt 50 ]]; then
+            wpctl set-volume @DEFAULT_AUDIO_SINK@ 50%
+        fi
+        wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+        ;;
+    *)
+        return
+        ;;
+    esac
 
-	parse_volume
-	parse_notify_title
-	parse_volume_icon
-	send_notify
+    parse_volume
+    parse_notify_title
+    parse_volume_icon
+    send_notify
 }
 
 do_process
