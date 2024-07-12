dd3fc0f181e03475d1c21b82dd00dd4789372f83
lulkien <kien.luuhoang.arch@gmail.com>
2024-07-12 23:24:54 +0700

    Add SSH check for fish prompt

--- configs/fish/functions/fish_prompt.fish
+++ configs/fish/functions/fish_prompt.fish
@@ -1,22 +1,25 @@
 function fish_prompt --description 'Write out the prompt'
-    set -f _start_prompt $KFC_WHITE_B' '$KFC_CL_NONE
-    set -f _end_prompt $KFC_WHITE_B' '$KFC_CL_NONE
+    set prompt_string ''
 
-    set -f _user $KFC_CYAN_N' '$USER
-    set -f _cwd $KFC_BLUE_N' 󰉋 '(prompt_pwd --full-length-dirs=2 --dir-length=2)
-    set -f _git_str (kfc_git_prompt)
+    set user_string $KFC_CYAN_N' '(whoami)
+    set cwd_string $KFC_BLUE_N' 󰉋 '(prompt_pwd --full-length-dirs=2 --dir-length=2)
+    set git_string (kfc_git_prompt)
 
-    set -f _cmd_prefix $KFC_WHITE_N' 󰶻  '$KFC_CL_NONE
+    set command_prefix $KFC_WHITE_N' 󰶻  '$KFC_CL_NONE
 
     # OVERRIDE ROOT PREFIX IS A BAD IDEA, TRUST ME
-    if test $USER = root
-        set _cmd_prefix $KFC_RED_N' # 󰶻  '$KFC_CL_NONE
+    if test (whoami) = root
+        set command_prefix $KFC_RED_N' # 󰶻  '$KFC_CL_NONE
     end
 
-    # Construct prompt
-    set -f _prompt_string $_start_prompt$_user$_cwd$_git_str$_end_prompt
+    # Check being remoted via ssh
+    if set -q SSH_TTY
+        set prompt_string $KFC_RED_N"[Remote]"
+    end
+
+    set -a prompt_string $user_string$cwd_string$git_string
 
     # Output
-    printf "$_prompt_string\n"
-    printf "$_cmd_prefix"
+    printf "$prompt_string\n"
+    printf "$command_prefix"
 end
