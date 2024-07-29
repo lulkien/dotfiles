80d7cde5aa4c6b7e40dba21235eb6dd11f4b4482
lulkien <kien.luuhoang.arch@gmail.com>
2024-07-29 23:26:40 +0700

    Change some config

--- configs/nvim/lua/configs/lspconfig.lua
+++ configs/nvim/lua/configs/lspconfig.lua
@@ -5,29 +5,28 @@ local lspconfig = require("lspconfig")
 
 -- Table of LSP servers
 local language_servers = {
-    "bashls",
-    "cmake",
-    "cssls",
-    "jsonls",
-    "lua_ls",
-    "slint_lsp",
-    "pyright",
-    "tsserver",
+	"bashls",
+	"cssls",
+	"jsonls",
+	"lua_ls",
+	"slint_lsp",
+	"pyright",
+	"tsserver",
 }
 
 -- Default configs for lsp
 for _, lsp in ipairs(language_servers) do
-    lspconfig[lsp].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-    })
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
 -- Config for clangd --
 lspconfig.clangd.setup({
-    on_attach = function(client, bufnr)
-        client.server_capabilities.signatureHelpProvider = false
-        on_attach(client, bufnr)
-    end,
-    capabilities = capabilities,
+	on_attach = function(client, bufnr)
+		client.server_capabilities.signatureHelpProvider = false
+		on_attach(client, bufnr)
+	end,
+	capabilities = capabilities,
 })
