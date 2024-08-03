78d65fbe53243f54323a53584020e52b4bf3a4d4
lulkien <kien.luuhoang.arch@gmail.com>
2024-08-03 22:01:35 +0700

    Change dunstrc, add command to capture screen

---
+++ configs/fish/functions/wl-capture-region.fish
@@ -0,0 +1,26 @@
+function wl-capture-region
+    if test "$XDG_SESSION_TYPE" != wayland
+        echo "This function can only run on wayland."
+        return 1
+    end
+
+    if not command -sq wf-recorder
+        echo "Command not found: wf-recorder"
+        return 2
+    end
+
+    if not command -sq slurp
+        echo "Command not found: slurp"
+        return 2
+    end
+
+    set -l record_region (slurp)
+    if test $status -ne 0
+        eecho Canceled.
+        return 4
+    end
+
+    echo "Execute: wf-recorder -g \"$record_region\""
+
+    wf-recorder -g "$record_region" $argv
+end
