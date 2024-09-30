7805f7a24e6f10a10cc7f1fb3f9488bcccbf3510
lulkien <kien.luuhoang.arch@gmail.com>
2024-09-30 20:38:30 +0700

    Change config

--- configs/nvim/lua/neovide.lua
+++ configs/nvim/lua/neovide.lua
@@ -19,7 +19,7 @@ if vim.g.neovide then
 	vim.g.neovide_light_angle_degrees = 45
 	vim.g.neovide_light_radius = 5
 
-	vim.g.neovide_transparency = 0.9
+	vim.g.neovide_transparency = 1.0
 
 	vim.g.neovide_position_animation_length = 0.15
 
