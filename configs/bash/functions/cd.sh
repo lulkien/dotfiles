6ca83f35614b7f61d0160b109d81e8793abe35ba
lulkien <kien.luuhoang.arch@gmail.com>
2024-11-06 00:50:58 +0700

    Rework on cd.sh

--- configs/bash/functions/cd.sh
+++ configs/bash/functions/cd.sh
@@ -1,8 +1,8 @@
 function cd() {
     # Global variables for this SHELL session
-    declare -p BASH_CD_HISTORY &>/dev/null || export BASH_CD_HISTORY=
-    declare -p BASH_CD_PREV &>/dev/null || export BASH_CD_PREV=$HOME
-    declare -p BASH_CD_HISTORY_LEN &>/dev/null || export BASH_CD_HISTORY_LEN=15
+    declare -p KBC_CD_HISTORY &>/dev/null || export KBC_CD_HISTORY=()
+    declare -p KBC_CD_PREV &>/dev/null || export KBC_CD_PREV=$HOME
+    declare -p KBC_CD_HISTORY_LEN &>/dev/null || export KBC_CD_HISTORY_LEN=15
 
     __cd_print_help() {
         echo "Usage: cd [OPTION]"
@@ -10,8 +10,7 @@ function cd() {
         echo "Change directory to a specific path or a history index."
         echo
         echo "Options:"
-        echo "    -i, --index=index     Jump to INDEX of history"
-        echo "    -l, --list            Print cd history list"
+        echo "    -l, --list            Show cd history list and jump"
         echo "    -h, --help            Show help"
     }
 
@@ -19,36 +18,20 @@ function cd() {
         [[ $@ =~ ^[0-9]+$ ]]
     }
 
-    __cd_prepend_history() {
-        [[ -z "$1" ]] && return
-
-        [[ -z "$BASH_CD_HISTORY" ]] &&            # Condition
-            BASH_CD_HISTORY="$1" ||               # Do if true
-            BASH_CD_HISTORY="$1:$BASH_CD_HISTORY" # Do if false
-    }
-
-    __cd_append_history() {
-        [[ -z "$1" ]] && return
-
-        [[ -z "$BASH_CD_HISTORY" ]] &&            # Condition
-            BASH_CD_HISTORY="$1" ||               # Do if true
-            BASH_CD_HISTORY="$BASH_CD_HISTORY:$1" # Do if false
-    }
-
     __cd_update_history() {
         [[ -z "$1" || "$1" = "$HOME" ]] && return
-        __cd_prepend_history "$1"
+
+        KBC_CD_HISTORY=("$1" "${KBC_CD_HISTORY[@]}")
     }
 
     __cd_remove_history() {
         [[ -z "$1" || "$1" = "$HOME" ]] && return
 
         # Save tmp history
-        local tmp_history=
-        IFS=: read -r -a tmp_history <<<"$BASH_CD_HISTORY"
+        local tmp_history=("${KBC_CD_HISTORY[@]}")
 
         # Clean history
-        BASH_CD_HISTORY=
+        KBC_CD_HISTORY=()
         local history_len=0
 
         # Update history
@@ -56,59 +39,52 @@ function cd() {
             [[ $item = $1 ]] && continue
 
             history_len=$(($history_len + 1))
-            [[ $history_len -ge $BASH_CD_HISTORY_LEN ]] && break # Overflow
+            [[ $history_len -ge $KBC_CD_HISTORY_LEN ]] && break # Overflow
 
-            __cd_append_history "$item"
+            KBC_CD_HISTORY+=("$item")
         done
     }
 
-    __cd_print_history() {
-        local index=0
-        local tmp_history=
-
-        IFS=: read -r -a tmp_history <<<"$BASH_CD_HISTORY"
-
-        for item in "${tmp_history[@]}"; do
-            echo "$index - $item"
+    __cd_print_list() {
+        local index=1
+        for history in "${KBC_CD_HISTORY[@]}"; do
+            echo "${index} - ${history}"
             index=$(($index + 1))
         done
     }
 
     __cd_jump() {
-        local jump_index="$1"
-        local history_len=$(echo $BASH_CD_HISTORY | awk -F: '{print NF}')
+        local history_len="${#KBC_CD_HISTORY[@]}"
 
-        if [[ $jump_index -eq 0 ]] && [[ $history_len -eq 0 ]]; then
-            echo "$BASH_CD_PREV"
+        COLUMNS=30
+        select history_dir in "${KBC_CD_HISTORY[@]}"; do
+
+            if [[ ! "$REPLY" =~ ^[0-9]+$ ]]; then
+                echo "Index must be a number."
+                return 2
+            fi
+
+            if [[ "$REPLY" -lt 1 || "$REPLY" -gt $history_len ]]; then
+                echo "Index out of range."
+                return 2
+            fi
+
+            echo "$history_dir"
             return 0
-        fi
 
-        if [[ $jump_index -ge $history_len ]]; then
-            echo 'Invalid index.'
-            return 1
-        fi
-
-        local tmp_history=
-        IFS=: read -r -a tmp_history <<<"$BASH_CD_HISTORY"
-
-        echo "${tmp_history[$_flag_index]}"
-        return 0
+        done
     }
 
     # ----------------------------------- MAIN -----------------------------------
     # Variables for CD script
     cd_destination=
     _flag_help=false
-    _flag_index=
+    _flag_jump=false
     _flag_list=false
     argument_count=0
 
     while [[ $# -gt 0 ]]; do
         case $1 in
-        -)
-            cd_destination=$BASH_CD_PREV
-            argument_count=$(($argument_count + 1))
-            ;;
         -h | --help)
             _flag_help=true
             argument_count=$(($argument_count + 1))
@@ -117,13 +93,8 @@ function cd() {
             _flag_list=true
             argument_count=$(($argument_count + 1))
             ;;
-        -i | --index)
-            _flag_index="$2"
-            argument_count=$(($argument_count + 1))
-            shift
-            ;;
-        -i* | --index=* | -*)
-            _flag_index=${1##*[-=i]} # remove everything until the last char '-' or '=' or 'i'
+        -j | --jump)
+            _flag_jump=true
             argument_count=$(($argument_count + 1))
             ;;
         *)
@@ -145,48 +116,44 @@ function cd() {
     fi
 
     if $_flag_list; then
-        __cd_print_history
+        __cd_print_list
         return 0
     fi
 
-    if [[ -n "$_flag_index" ]]; then
-        if ! __cd_number_check $_flag_index; then
-            echo "cd: Index must be a number: $_flag_index"
-            return 2
+    if $_flag_jump; then
+        local jump_msg=
+        local jump_err=
+        jump_msg=$(__cd_jump)
+        jump_err=$?
+        if [[ $jump_err -ne 0 ]]; then
+            echo "cd: $jump_msg"
+            return $jump_err
         fi
 
-        local jump_data=
-        local jump_code=
-
-        jump_data=$(__cd_jump $_flag_index)
-        jump_code=$?
-
-        if [[ $jump_code -ne 0 ]]; then
-            echo "cd: $jump_data"
-            return 3
+        cd_destination="$jump_msg"
+        echo "----------------------------"
+        echo -e "\e[1;33mJump to\e[00m: $cd_destination"
+    else
+        if [[ -z "$cd_destination" ]]; then
+            cd_destination="$HOME"
+        elif [[ "$cd_destination" = '-' ]]; then
+            cd_destination="$KBC_CD_PREV"
+        else
+            local real_path=
+            real_path=$(realpath -q "$cd_destination")
+            [[ $? -eq 0 ]] && cd_destination="$real_path"
         fi
-
-        cd_destination=$jump_data
     fi
-
-    [[ -z "$cd_destination" ]] && cd_destination=$HOME
 
     local current_dir=$(pwd)
     [[ "$current_dir" = "$cd_destination" ]] && return 0
 
-    local real_path=
-    real_path=$(realpath -q $cd_destination)
-
-    [[ $? -eq 0 ]] && cd_destination=$real_path
-
-    [[ -n "$_flag_index" ]] && echo "cd $cd_destination"
-
-    builtin cd -- $cd_destination
+    builtin cd -- "$cd_destination"
 
     if [[ $? -eq 0 ]]; then
-        __cd_remove_history $cd_destination
-        BASH_CD_PREV=$current_dir
-        __cd_update_history $current_dir
+        __cd_remove_history "$cd_destination"
+        KBC_CD_PREV="$current_dir"
+        __cd_update_history "$current_dir"
     fi
 
     return $cd_status
