f971120947675afbedc1d9855563207aa2a3bfc9
lulkien <kien.luuhoang.arch@gmail.com>
2024-11-10 01:01:38 +0700

    Modify script volume_ctrl

--- configs/hypr/scripts/volume_ctrl.sh
+++ configs/hypr/scripts/volume_ctrl.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-CMD="$1"
-
 VOLUME=0
 MUTED=false
+MAXIMUM_VOLUME=0.4
 
 # For dunstify
 NOTIFY_ID=865863
@@ -14,38 +13,26 @@ NOTIFY_ICON=
 ICON_PATH_PREFIX='/usr/share/icons/Papirus/48x48/status/notification-audio-volume-'
 ICON_PATH_SUFFIX='.svg'
 
-if [[ -z "$CMD" || ! $(command -v wpctl) ]]; then
-    exit 1
-fi
-
 parse_volume() {
     # Extract volume value
     local volume_data=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
-    if [[ -z "$volume_data" ]]; then
-        exit 1
-    fi
+    [[ -z "$volume_data" ]] && return 1
 
     local volume=$(echo $volume_data | cut -d ' ' -f 2)
     local mute_grep=$(echo $volume_data | grep 'MUTED')
 
     VOLUME=$(awk "BEGIN {print $volume * 100}")
-    if [[ -z "$mute_grep" ]]; then
-        MUTED=false
-    else
+    [[ -z "$mute_grep" ]] &&
+        MUTED=false ||
         MUTED=true
-    fi
 }
 
 parse_notify_title() {
-    if [[ $MUTED = true ]]; then
-        NOTIFY_TITLE="Muted"
-    else
-        NOTIFY_TITLE="Volume"
-    fi
+    $MUTED && NOTIFY_TITLE="Muted" || NOTIFY_TITLE="Volume"
 }
 
 parse_volume_icon() {
-    if [[ $MUTED = true || $VOLUME -eq 0 ]]; then
+    if $MUTED || [[ $VOLUME -eq 0 ]]; then
         NOTIFY_ICON="${ICON_PATH_PREFIX}muted${ICON_PATH_SUFFIX}"
         return
     fi
@@ -60,37 +47,44 @@ parse_volume_icon() {
 }
 
 send_notify() {
-    if [[ ! $(command -v dunstify) ]]; then
-        exit 1
-    fi
+    [[ $(command -v dunstify) ]] || return 1
     dunstify --replace=$NOTIFY_ID --timeout=$NOTIFY_TIMEOUT --icon=$NOTIFY_ICON --hints=int:value:"$VOLUME%" "$NOTIFY_TITLE" "$VOLUME%"
 }
 
-do_process() {
-    parse_volume
+play_sound() {
+    pw-play /usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
+}
 
-    case "$CMD" in
-    '+' | 'inc' | 'INC')
-        wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+
+do_process() {
+    [[ -z "$1" ]] && return 1
+    [[ $(command -v wpctl) ]] || return 1
+
+    local cmd=${1,,}
+
+    parse_volume || return 1
+
+    case "$cmd" in
+    '+' | 'inc')
+        wpctl set-volume -l $MAXIMUM_VOLUME @DEFAULT_AUDIO_SINK@ 5%+
+        play_sound
         ;;
-    '-' | 'dec' | 'DEC')
-        wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%-
+    '-' | 'dec')
+        wpctl set-volume -l $MAXIMUM_VOLUME @DEFAULT_AUDIO_SINK@ 5%-
+        play_sound
         ;;
-    '0' | 'toggle' | 'TOGGLE')
-        if [[ $MUTED = true && $VOLUME -gt 50 ]]; then
+    'toggle')
+        if $MUTED && [[ $VOLUME -gt 50 ]]; then
             wpctl set-volume @DEFAULT_AUDIO_SINK@ 50%
         fi
         wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
         ;;
-    *)
-        return
-        ;;
+    *) ;;
     esac
 
-    parse_volume
-    parse_notify_title
-    parse_volume_icon
-    send_notify
+    parse_volume || return 1
+    parse_notify_title || return 1
+    parse_volume_icon || return 1
+    send_notify || return 1
 }
 
-do_process
+do_process "$@"
