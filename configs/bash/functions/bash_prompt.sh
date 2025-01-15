fdf8d26ae669aa327b712fec7f778731039e12e2
lulkien <kien.luuhoang.arch@gmail.com>
2025-01-15 21:47:45 +0700

    feat: add the way to show hostname

--- configs/bash/functions/bash_prompt.sh
+++ configs/bash/functions/bash_prompt.sh
@@ -1,4 +1,13 @@
 function bash_prompt() {
     declare -p SSH_TTY &>/dev/null && KBC_SESSION='\e[31m \e[00m' || KBC_SESSION=''
-    echo $KBC_SESSION'\e[1;36m  \u \e[1;34m󰉋 `cwd`\e[00m `bash_git_prompt`\n  '
+
+    if [[ "$KBC_SHOW_HOSTNAME" =~ ^(true|yes|ok|1)$ ]]; then
+        if [[ -n "$KBC_OVERRIDE_HOSTNAME" ]]; then
+            KBC_HOSTNAME='\e[1;33m 󰍹 '$KBC_OVERRIDE_HOSTNAME
+        else
+            KBC_HOSTNAME='\e[1;33m 󰍹 '$(hostname)
+        fi
+    fi
+
+    echo $KBC_SESSION'\e[1;36m  \u'$KBC_HOSTNAME' \e[1;34m󰉋 `cwd`\e[00m `bash_git_prompt`\n  '
 }
