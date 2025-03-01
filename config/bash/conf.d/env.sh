9b393b353c38e02692f2b8cbd290f81b522633bd
lulkien <kien.luuhoang.arch@outlook.com>
2025-03-01 23:50:06 +0700

    Update config

--- config/bash/conf.d/env.sh
+++ config/bash/conf.d/env.sh
@@ -1,1 +1,7 @@
-export PATH=$HOME/.local/bin:$PATH
+KBC_USER_PATH=("$HOME/.local/bin" "$HOME/.cargo/bin" "$HOME/go/bin")
+
+for path in "${KBC_USER_PATH[@]}"; do
+    if [[ ! $PATH =~ $path ]]; then
+        export PATH=$path:$PATH
+    fi
+done
