d90fcf90a36e5d0aca562bd20c9fc13993260b9e
lulkien <kien.luuhoang.arch@outlook.com>
2025-03-06 01:17:34 +0700

    change bash and fish root user color

--- config/bash/functions/bash_prompt.sh
+++ config/bash/functions/bash_prompt.sh
@@ -1,6 +1,11 @@
 bash_prompt() {
     declare -p SSH_TTY &>/dev/null && KBC_SESSION='\e[1;31m \e[00m' || KBC_SESSION=''
 
+    KBC_USER='\e[0;36m  \u'
+    if [[ $(whoami) = 'root' ]]; then
+        KBC_USER='\e[0;31m  \u'
+    fi
+
     if [[ "$KBC_SHOW_HOSTNAME" =~ ^(true|yes|ok|1)$ ]]; then
         if [[ -n "$KBC_OVERRIDE_HOSTNAME" ]]; then
             KBC_HOSTNAME='\e[0;33m 󰍹 '$KBC_OVERRIDE_HOSTNAME
@@ -9,5 +14,5 @@ bash_prompt() {
         fi
     fi
 
-    echo $KBC_SESSION'\e[0;36m  \u'$KBC_HOSTNAME' \e[0;34m󰉋 `cwd`\e[00m `bash_git_prompt`\n  '
+    echo ${KBC_SESSION}${KBC_USER}${KBC_HOSTNAME}' \e[0;34m󰉋 `cwd`\e[00m `bash_git_prompt`\n  '
 }
--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -2,16 +2,15 @@ function fish_prompt --description 'Write out the prompt'
     set prompt_string ''
 
     set user_string $KFC_CYAN_N' '(whoami)
+    if test (whoami) = root
+        set user_string $KFC_RED_N' '(whoami)
+    end
+
     set host_string ''
     set cwd_string $KFC_BLUE_N' 󰉋 '(prompt_pwd --full-length-dirs=2 --dir-length=3)
     set git_string (kfc_git_prompt)
 
-    set command_prefix $KFC_GREEN_N'  '$KFC_CL_NONE
-
-    # OVERRIDE ROOT PREFIX IS A BAD IDEA, TRUST ME
-    if test (whoami) = root
-        set command_prefix $KFC_RED_N' #  '$KFC_CL_NONE
-    end
+    set command_prefix $KFC_CL_NONE'  '
 
     # Check being remoted via ssh
     if set -q SSH_TTY
