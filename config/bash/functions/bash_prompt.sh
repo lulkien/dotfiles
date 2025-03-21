49db11abc1081f1f4b68cd8074890cf0f39be627
lulkien <kien.luuhoang.arch@outlook.com>
2025-03-22 00:52:05 +0700

    chore(prompt): update

--- config/bash/functions/bash_prompt.sh
+++ config/bash/functions/bash_prompt.sh
@@ -1,18 +1,20 @@
 bash_prompt() {
-    declare -p SSH_TTY &>/dev/null && KBC_SESSION='\e[1;31m \e[00m' || KBC_SESSION=''
-
     KBC_USER='\e[0;36m  \u'
-    if [[ $(whoami) = 'root' ]]; then
+    if [[ $USER = 'root' ]]; then
         KBC_USER='\e[0;31m  \u'
     fi
 
     if [[ "$KBC_SHOW_HOSTNAME" =~ ^(true|yes|ok|1)$ ]]; then
+        declare -p SSH_TTY &>/dev/null &&
+            KBC_HOST_ICO='' ||
+            KBC_HOST_ICO='󰍹'
+
         if [[ -n "$KBC_OVERRIDE_HOSTNAME" ]]; then
-            KBC_HOSTNAME='\e[0;33m 󰍹 '$KBC_OVERRIDE_HOSTNAME
+            KBC_HOSTNAME='\e[0;33m '$KBC_HOST_ICO' '$KBC_OVERRIDE_HOSTNAME
         else
-            KBC_HOSTNAME='\e[0;33m 󰍹 '$(hostname)
+            KBC_HOSTNAME='\e[0;33m '$KBC_HOST_ICO' '$HOSTNAME
         fi
     fi
 
-    echo ${KBC_SESSION}${KBC_USER}${KBC_HOSTNAME}' \e[0;34m󰉋 `cwd`\e[00m `bash_git_prompt`\n  '
+    echo ${KBC_USER}${KBC_HOSTNAME}' \e[0;34m󰉋 `cwd`\e[00m `bash_git_prompt`\n  '
 }
--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,34 +1,43 @@
 function fish_prompt --description 'Write out the prompt'
-    set prompt_string ''
+    set padding_string ' '
+    set ico_dir '󰉋'
+    set ico_fish ''
+    set ico_host '󰍹'
+    set ico_ssh ''
+    set ico_user ''
 
-    set user_string $KFC_CYAN_N' '(whoami)
-    if test (whoami) = root
-        set user_string $KFC_RED_N' '(whoami)
-    end
+    # User string
+    test $USER = root
+    and set user_string $KFC_RED_N$ico_user
+    or set user_string $KFC_CYAN_N$ico_user
 
-    set host_string ''
-    set cwd_string $KFC_BLUE_N' 󰉋 '(prompt_pwd --full-length-dirs=2 --dir-length=3)
+    set -a user_string $USER
+
+    # Working directory string
+    set cwd_string $KFC_BLUE_N'󰉋 '(prompt_pwd --full-length-dirs=2 --dir-length=3)
+
+    # Git string
     set git_string (kfc_git_prompt)
 
-    set command_prefix $KFC_CL_NONE'  '
-
-    # Check being remoted via ssh
-    if set -q SSH_TTY
-        set prompt_string $KFC_RED_N" "
-    end
-
     if string match --regex --quiet -- '^(true|yes|ok|1)$' "$KFC_SHOW_HOSTNAME"
-        if test -n "$KFC_OVERRIDE_HOSTNAME"
-            set host_string $KFC_YELLOW_N' 󰍹 '$KFC_OVERRIDE_HOSTNAME
+        set -q SSH_TTY
+        and set host_string $KFC_YELLOW_N''
+        or set host_string $KFC_YELLOW_N'󰍹'
+
+        if set -q KFC_OVERRIDE_HOSTNAME; and test -n "$KFC_OVERRIDE_HOSTNAME"
+            set -a host_string $KFC_OVERRIDE_HOSTNAME
         else
-            set host_string $KFC_YELLOW_N' 󰍹 '(hostname)
+            set -a host_string $hostname
         end
     end
 
-    set -a prompt_string $user_string$host_string$cwd_string
+    set -a prompt_string "$user_string"
+    set -a prompt_string "$host_string"
+    set -a prompt_string "$cwd_string"
     set -a prompt_string "$git_string"
 
+    set -a command_prefix $KFC_CL_NONE' '
+
     # Output
-    printf "$prompt_string\n"
-    printf "$command_prefix"
+    printf "$padding_string$prompt_string\n$padding_string$command_prefix"
 end
