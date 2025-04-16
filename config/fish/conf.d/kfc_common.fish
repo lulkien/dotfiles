b450c606f0e1ecbac77b9ae4444ed7b0f6b8571a
lulkien <kien.luuhoang.arch@outlook.com>
2025-04-16 21:15:19 +0700

    fish: add some abbr

--- config/fish/conf.d/kfc_common.fish
+++ config/fish/conf.d/kfc_common.fish
@@ -20,13 +20,6 @@ if command -sq nvim
     set -x EDITOR nvim
 end
 
-# Abbreviations
-abbr -a chmox 'chmod +x'
-abbr -a rmc 'rm ~/.config/fish/fish_variables'
-abbr -a rf 'rm -rf'
-abbr -a ta 'tmux attach'
-abbr -a tl 'tmux ls'
-
 # Fish builtin variables
 set -g fish_color_valid_path
 set -g fish_term24bit 1
