effd6735350469352800d5ff2e4bc703a5b47b02
lulkien <kien.luuhoang.arch@gmail.com>
2025-04-24 19:04:35 +0700

    nvim: modify code actions popup

--- config/nvim/lua/plugins/fzf.lua
+++ config/nvim/lua/plugins/fzf.lua
@@ -21,6 +21,6 @@ return {
     vim.keymap.set("n", "<leader>ft", require("fzf-lua").btags, { desc = "Fzf buffer tags" })
     vim.keymap.set("n", "<leader>fT", require("fzf-lua").tags, { desc = "Fzf project tags" })
 
-    vim.keymap.set("n", "gca", require("fzf-lua").lsp_code_actions(), { desc = "LSP: Code Actions" })
+    vim.keymap.set("n", "lca", require("fzf-lua").lsp_code_actions, { desc = "LSP: Code Actions" })
   end,
 }
