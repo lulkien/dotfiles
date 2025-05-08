9aff4cb1cf564234d22631dd11c9f182ff6f3e60
lulkien <kien.luuhoang.arch@gmail.com>
2025-05-08 14:35:47 +0700

    zellij: add abbr

---
+++ config/fish/conf.d/kfc_zellij.fish
@@ -0,0 +1,9 @@
+if command -sq zellij
+    abbr -a -- zz zellij
+    abbr -a -- za 'zellij attach'
+    abbr -a -- zd 'zellij delete-session'
+    abbr -a -- zda 'zellij delete-all-sessions'
+    abbr -a -- zl 'zellij list-sessions'
+    abbr -a -- zk 'zellij kill-session'
+    abbr -a -- zka 'zellij kill-all-sessions'
+end
