dd1da7f1985e7e783226d7e2a612475ef38fa527
Kien H. Luu <30395846+lulkien@users.noreply.github.com>
2025-05-29 09:01:55 +0000

    nvim: cleanup

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -1,73 +1,81 @@
 ---@type LazyConfig
-local M = {
-  {
-    -- Detect tabstop and shiftwidth automatically
-    "tpope/vim-sleuth",
-  },
-  {
-    -- Powerful Git integration for Vim
-    "tpope/vim-fugitive",
-  },
-  {
-    -- GitHub integration for vim-fugitive
-    "tpope/vim-rhubarb",
-  },
-  {
-    -- Autoclose parentheses, brackets, quotes, etc.
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
-    opts = {},
-  },
-  {
-    -- Highlight todo, notes, etc in comments
-    "folke/todo-comments.nvim",
-    event = "VimEnter",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = { signs = false },
-  },
-  {
-    -- High-performance color highlighter
-    "norcalli/nvim-colorizer.lua",
-    config = function()
-      require("colorizer").setup()
-    end,
-  },
-  -- {
-  --   -- Notification
-  --   "echasnovski/mini.notify",
-  --   version = "*",
-  --   config = function()
-  --     require("mini.notify").setup({
-  --       lsp_progress = {
-  --         enable = true,
-  --         level = "INFO",
-  --         duration_last = 1000,
-  --       },
-  --     })
-  --   end,
-  -- },
-  {
-    "mrded/nvim-lsp-notify",
-    dependencies = {
-      {
-        "rcarriga/nvim-notify",
-        config = function()
-          ---@diagnostic disable-next-line: assign-type-mismatch
-          vim.notify = require("notify")
-        end,
-      },
-    },
-    config = function()
-      require("lsp-notify").setup()
-    end,
-  },
-  {
-    "m4xshen/hardtime.nvim",
-    lazy = false,
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {},
-  },
+local sleuth = {
+  -- Detect tabstop and shiftwidth automatically
+  [1] = "tpope/vim-sleuth",
 }
 
-return M
+---@type LazyConfig
+local fugitive = {
+  -- Powerful Git integration for Vim
+  [1] = "tpope/vim-fugitive",
+}
+
+---@type LazyConfig
+local rhubarb = {
+  -- GitHub integration for vim-fugitive
+  [1] = "tpope/vim-rhubarb",
+}
+
+---@type LazyConfig
+local autopairs = {
+  -- Autoclose parentheses, brackets, quotes, etc.
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = true,
+  opts = {},
+}
+
+---@type LazyConfig
+local comments = {
+  -- Highlight todo, notes, etc in comments
+  "folke/todo-comments.nvim",
+  event = "VimEnter",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = { signs = false },
+}
+
+---@type LazyConfig
+local colorizer = {
+  -- High-performance color highlighter
+  "norcalli/nvim-colorizer.lua",
+  config = function()
+    require("colorizer").setup()
+  end,
+}
+
+---@type LazyConfig
+local notify = {
+  "mrded/nvim-lsp-notify",
+  dependencies = {
+    {
+      "rcarriga/nvim-notify",
+      config = function()
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        vim.notify = require("notify")
+      end,
+    },
+  },
+  config = function()
+    require("lsp-notify").setup()
+  end,
+}
+
+---@type LazyConfig
+local hardtime = {
+  "m4xshen/hardtime.nvim",
+  lazy = false,
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {},
+}
+
+---@type LazyConfig[]
+return {
+  sleuth,
+  fugitive,
+  rhubarb,
+  autopairs,
+  comments,
+  colorizer,
+  notify,
+  hardtime,
+}
