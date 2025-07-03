32207a374e416ac3d9f88594067f89e7a2ba2f1b
lulkien <kien.luuhoang.arch@gmail.com>
2025-07-04 01:37:28 +0700

    fish: change config

--- config/fish/conf.d/kfc_abbr.fish
+++ config/fish/conf.d/kfc_abbr.fish
@@ -3,10 +3,5 @@ abbr -a chmox 'chmod +x'
 abbr -a rmc 'rm ~/.config/fish/fish_variables'
 abbr -a rf 'rm -rf'
 
-# Tmux
-abbr -a tn 'tmux new'
-abbr -a ta 'tmux attach'
-abbr -a tl 'tmux ls'
-
 # Misc
 abbr -a ipa 'ip address'
--- config/fish/conf.d/kfc_common.fish
+++ config/fish/conf.d/kfc_common.fish
@@ -1,19 +1,14 @@
-# Append some paths into PATH variable
-if not contains "$HOME/.cargo/bin" $PATH
-    set -xp PATH $HOME/.cargo/bin
+function prepend_path
+    test -z "$argv[1]"; and return # $argv[1] is zero string
+    test -d "$argv[1]"; or return # $argv[1] is not a directory
+    contains "$argv[1]" $PATH; or set -xp PATH "$argv[1]" # $PATH does not contains $argv[1]
 end
 
-if not contains "$HOME/go/bin" $PATH
-    set -xp PATH $HOME/go/bin
-end
-
-if not contains "$HOME/.local/bin" $PATH
-    set -xp PATH $HOME/.local/bin
-end
-
-if not contains /opt/homebrew/bin $PATH
-    set -xp PATH /opt/homebrew/bin
-end
+prepend_path "$HOME/.local/bin"
+prepend_path "$HOME/go/bin"
+prepend_path "$HOME/cargo/bin"
+prepend_path /opt/homebrew/bin
+prepend_path /home/linuxbrew/.linuxbrew/bin
 
 # Set editor
 if command -sq nvim
@@ -28,4 +23,5 @@ set -g fish_greeting
 # Recommend for user config
 # set -g KFC_SHOW_GIT_STATUS true
 # set -g KFC_SHOW_GIT_RELATIVE true
+# set -g KFC_SHOW_HOSTNAME true
 # set -g fish_greeting    $KFC_GREEN_B"Your greeting message here"
