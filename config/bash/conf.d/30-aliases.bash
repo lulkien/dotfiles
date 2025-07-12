4ade3267fd29b0dd014dc72e359e23b2db63bba6
lulkien <kien.luuhoang.arch@outlook.com>
2025-07-12 22:14:32 +0700

    bash: change config

--- config/bash/conf.d/30-aliases.bash
+++ config/bash/conf.d/30-aliases.bash
@@ -7,13 +7,3 @@ alias rf='rm -rf'
 
 alias ..='cd ..'
 alias ...='cd ../..'
-
-if command -v zellij &>/dev/null; then
-    alias zn="zellij"
-    alias zl="zellij list-sessions"
-    alias za="zellij attach"
-    alias zk="zellij kill-session"
-    alias zka="zellij kill-all-sessions"
-    alias zd="zellij delete-session"
-    alias zda="zellij delete-all-sessions"
-fi
