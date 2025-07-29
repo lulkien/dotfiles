5c07a279ab7835386ca2932177820923a5566135
lulkien <kien.luuhoang.arch@gmail.com>
2025-07-30 00:16:29 +0700

    bash: Set default PATH

--- config/bash/conf.d/10-env.bash
+++ config/bash/conf.d/10-env.bash
@@ -1,11 +1,15 @@
 bash_prepend_path() {
-    [ -z "$1" ] && return
-    [ -d "$1" ] || return
-    [[ $PATH =~ "$1" ]] && return
+	[ -z "$1" ] && return
+	[ -d "$1" ] || return
+	[[ $PATH =~ "$1" ]] && return
 
-    export PATH="$1:$PATH"
+	export PATH="$1:$PATH"
 }
 
-bash_prepend_path $HOME/.local/bin
-bash_prepend_path $HOME/.cargo/bin
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
+
 bash_prepend_path /home/linuxbrew/.linuxbrew/bin
+bash_prepend_path /home/linuxbrew/.linuxbrew/sbin
+
+bash_prepend_path $HOME/.cargo/bin
+bash_prepend_path $HOME/.local/bin
