76f7e63c02af534b2c9972a66d50f01daee59bc5
lulkien <kien.luuhoang.arch@gmail.com>
2025-09-07 20:20:48 +0700

    bash: unset default PS1

--- config/bash/rc.bash
+++ config/bash/rc.bash
@@ -1,16 +1,15 @@
 RUNTIME_DIR=$HOME/.config/bash
 
 for conf in $(find "$RUNTIME_DIR/conf.d/rc" -type f -name '*.bash' | sort); do
-	. $conf
+    . $conf
 done
 
 for func in $(find "$RUNTIME_DIR/functions" -type f -name '*.bash' | sort); do
-	. $func
+    . $func
 done
 
 for comp in $(find "$RUNTIME_DIR/completion" -type f -name '*.bash' | sort); do
-	. $comp
+    . $comp
 done
 
 bash_greeting
-PS1=$(bash_prompt)
