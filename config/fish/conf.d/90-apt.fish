980211c17d8d892fb88a700e80b80cd5503939d4
lulkien <kien.luuhoang.arch@gmail.com>
2025-09-14 23:41:39 +0700

    fish: update config

--- config/fish/conf.d/90-apt.fish
+++ config/fish/conf.d/90-apt.fish
@@ -2,15 +2,15 @@ if not command -sq apt
     return
 end
 
-set -l apt_wrapper apt
-if command -sq apt-fast
-    set apt_wrapper apt-fast
+set -l APT_CMD 'sudo apt'
+
+if (whoami) = root
+    set APT_CMD apt
 end
 
-abbr -a pit "sudo $apt_wrapper install -y"
-abbr -a pfm "sudo $apt_wrapper install --fix-missing"
-abbr -a prm 'sudo apt purge'
-abbr -a puf "sudo apt update; sudo $apt_wrapper upgrade"
-abbr -a pcl 'sudo apt autoclean; sudo apt autoremove'
+abbr -a pit "$APT_CMD install -y"
+abbr -a prm "$APT_CMD purge"
+abbr -a puf "$APT_CMD update; $APT_CMD upgrade"
+abbr -a pcl "$APT_CMD autoclean; $APT_CMD autoremove"
 
 abbr -a pss 'apt-cache search'
