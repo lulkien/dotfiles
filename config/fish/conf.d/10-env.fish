59d2c51013fd90b2fdd194dfa0a115e91836e3c0
lulkien <kien.luuhoang.arch@outlook.com>
2025-10-16 04:10:49 +0000

    fish: update config

--- config/fish/conf.d/10-env.fish
+++ config/fish/conf.d/10-env.fish
@@ -1,21 +1,14 @@
-function prepend_path
+function fish_add_path_if_existed 
     test -z "$argv[1]"; and return # $argv[1] is zero string
     test -d "$argv[1]"; or return # $argv[1] is not a directory
-    contains "$argv[1]" $PATH; or set -xp PATH "$argv[1]" # $PATH does not contains $argv[1]
+    fish_add_path "$argv[1]"
 end
 
-# Reset PATH
-set -e PATH
-
-# Export default PATH
-set -gx PATH /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin
-
-# Prepend additional PATH
-prepend_path "$HOME/.local/bin"
-prepend_path "$HOME/go/bin"
-prepend_path "$HOME/.cargo/bin"
-prepend_path /opt/homebrew/bin
-prepend_path /home/linuxbrew/.linuxbrew/bin
+fish_add_path_if_existed "$HOME/.local/bin"
+fish_add_path_if_existed "$HOME/go/bin"
+fish_add_path_if_existed "$HOME/.cargo/bin"
+fish_add_path_if_existed /opt/homebrew/bin
+fish_add_path_if_existed /home/linuxbrew/.linuxbrew/bin
 
 # Fish builtin variables
 set -g fish_color_valid_path
