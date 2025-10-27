a769eb703cc3e656efee6e2bb90001af0f98bf56
lulkien <kien.luuhoang.arch@gmail.com>
2025-10-27 23:16:51 +0700

    bash: update config

--- config/bash/conf.d/10-aliases.bash
+++ config/bash/conf.d/10-aliases.bash
@@ -1,7 +1,9 @@
 alias pls=sudo
-alias please=sudo
 
 alias rf='rm -rf'
 
 alias ..='cd ..'
 alias ...='cd ../..'
+
+alias cls=clear
+alias q=exit
--- config/bash/conf.d/20-config.bash
+++ config/bash/conf.d/20-config.bash
@@ -3,3 +3,5 @@ export KBC_SHOW_GIT_STATUS=true
 
 export KBC_PROMPT_DIR_TRIM=2
 export KBC_PROMPT_DIR_SIZE=4
+
+export TERM=xterm-256color
