395ad2c1346178b9eb1bb051548cbb34d63964e4
lulkien <kien.luuhoang.arch@outlook.com>
2025-12-05 04:41:51 +0000

    nvim: update

--- config/nvim/lua/plugins/rustaceanvim.lua
+++ config/nvim/lua/plugins/rustaceanvim.lua
@@ -1,6 +1,19 @@
 ---@type LazyConfig
 return {
     "mrcjkb/rustaceanvim",
+    dependencies = {
+        {
+            "alexpasmantier/krust.nvim",
+            ft = "rust",
+            opts = {
+                keymap = "<leader>k",
+                float_win = {
+                    border = "rounded",
+                    auto_focus = false,
+                },
+            },
+        }
+    },
     version = "^6",
     lazy = false,
 }
